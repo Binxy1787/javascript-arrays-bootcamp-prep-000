d3689f82611b0bd3e4367ae403f6a2dee5051dd4
Jameson Nelson <nelson_jameson@yahoo.com>
2019-01-25 00:01:01 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,41 @@
+// define our array.
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element){
+  var addElement = element + ",";
+  var myArray = addElement.concat(array);
+  
+  
+
+  //var myArray = [element, ...array];
+  return myArray;
+}
+
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  
+}
+
+function addElementToEndOfArray(array, element){
+  
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  
+}
+
+function accessElementInArray(array, index){
+  
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  
+}
+
+function removeElementFromEndOfArray(array){
+  
+}
